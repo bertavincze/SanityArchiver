75e2af656fbb4c4d1771e56325f2293add1b2a7a
Berta Vincze <berta.vincze@gmail.com>
2019-07-15 14:12:48 +0200

    Add functions to browse files and open selected file

--- SanityArchiver/SanityArchiver/SanityArchiver.Designer.cs
+++ SanityArchiver/SanityArchiver/SanityArchiver.Designer.cs
@@ -31,11 +31,18 @@
             this.SelectedFileName = new System.Windows.Forms.TextBox();
             this.SelectedFileLabel = new System.Windows.Forms.Label();
             this.BrowseFilesButton = new System.Windows.Forms.Button();
+            this.OpenFileButton = new System.Windows.Forms.Button();
+            this.OptionsLabel = new System.Windows.Forms.Label();
+            this.OpenSelectedFileButton = new System.Windows.Forms.Button();
+            this.FileOptionsGroupBox = new System.Windows.Forms.GroupBox();
+            this.SearchGroupBox = new System.Windows.Forms.GroupBox();
+            this.FileOptionsGroupBox.SuspendLayout();
+            this.SearchGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // ArchiveButton
             // 
-            this.ArchiveButton.Location = new System.Drawing.Point(97, 105);
+            this.ArchiveButton.Location = new System.Drawing.Point(9, 84);
             this.ArchiveButton.Name = "ArchiveButton";
             this.ArchiveButton.Size = new System.Drawing.Size(75, 23);
             this.ArchiveButton.TabIndex = 1;
@@ -44,7 +51,7 @@
             // 
             // ExtractButton
             // 
-            this.ExtractButton.Location = new System.Drawing.Point(178, 105);
+            this.ExtractButton.Location = new System.Drawing.Point(9, 113);
             this.ExtractButton.Name = "ExtractButton";
             this.ExtractButton.Size = new System.Drawing.Size(75, 23);
             this.ExtractButton.TabIndex = 2;
@@ -53,7 +60,7 @@
             // 
             // EncryptButton
             // 
-            this.EncryptButton.Location = new System.Drawing.Point(259, 105);
+            this.EncryptButton.Location = new System.Drawing.Point(90, 84);
             this.EncryptButton.Name = "EncryptButton";
             this.EncryptButton.Size = new System.Drawing.Size(75, 23);
             this.EncryptButton.TabIndex = 3;
@@ -62,15 +69,15 @@
             // 
             // FileSearchBox
             // 
-            this.FileSearchBox.Location = new System.Drawing.Point(12, 26);
+            this.FileSearchBox.Location = new System.Drawing.Point(9, 32);
             this.FileSearchBox.Name = "FileSearchBox";
-            this.FileSearchBox.Size = new System.Drawing.Size(510, 20);
+            this.FileSearchBox.Size = new System.Drawing.Size(370, 20);
             this.FileSearchBox.TabIndex = 4;
             // 
             // SearchLabel
             // 
             this.SearchLabel.AutoSize = true;
-            this.SearchLabel.Location = new System.Drawing.Point(9, 10);
+            this.SearchLabel.Location = new System.Drawing.Point(6, 16);
             this.SearchLabel.Name = "SearchLabel";
             this.SearchLabel.Size = new System.Drawing.Size(44, 13);
             this.SearchLabel.TabIndex = 5;
@@ -78,7 +85,7 @@
             // 
             // DecryptButton
             // 
-            this.DecryptButton.Location = new System.Drawing.Point(340, 105);
+            this.DecryptButton.Location = new System.Drawing.Point(90, 113);
             this.DecryptButton.Name = "DecryptButton";
             this.DecryptButton.Size = new System.Drawing.Size(75, 23);
             this.DecryptButton.TabIndex = 6;
@@ -87,24 +94,25 @@
             // 
             // AttributesButton
             // 
-            this.AttributesButton.Location = new System.Drawing.Point(421, 105);
+            this.AttributesButton.Location = new System.Drawing.Point(171, 84);
             this.AttributesButton.Name = "AttributesButton";
-            this.AttributesButton.Size = new System.Drawing.Size(101, 23);
+            this.AttributesButton.Size = new System.Drawing.Size(75, 23);
             this.AttributesButton.TabIndex = 7;
-            this.AttributesButton.Text = "Manage Attributes";
+            this.AttributesButton.Text = "Attributes";
             this.AttributesButton.UseVisualStyleBackColor = true;
             // 
             // SelectedFileName
             // 
-            this.SelectedFileName.Location = new System.Drawing.Point(12, 67);
+            this.SelectedFileName.Location = new System.Drawing.Point(9, 32);
             this.SelectedFileName.Name = "SelectedFileName";
-            this.SelectedFileName.Size = new System.Drawing.Size(510, 20);
+            this.SelectedFileName.Size = new System.Drawing.Size(370, 20);
             this.SelectedFileName.TabIndex = 8;
+            this.SelectedFileName.TextChanged += new System.EventHandler(this.SelectedFileChanged);
             // 
             // SelectedFileLabel
             // 
             this.SelectedFileLabel.AutoSize = true;
-            this.SelectedFileLabel.Location = new System.Drawing.Point(9, 50);
+            this.SelectedFileLabel.Location = new System.Drawing.Point(6, 16);
             this.SelectedFileLabel.Name = "SelectedFileLabel";
             this.SelectedFileLabel.Size = new System.Drawing.Size(71, 13);
             this.SelectedFileLabel.TabIndex = 9;
@@ -112,7 +120,7 @@
             // 
             // BrowseFilesButton
             // 
-            this.BrowseFilesButton.Location = new System.Drawing.Point(12, 105);
+            this.BrowseFilesButton.Location = new System.Drawing.Point(94, 62);
             this.BrowseFilesButton.Name = "BrowseFilesButton";
             this.BrowseFilesButton.Size = new System.Drawing.Size(79, 23);
             this.BrowseFilesButton.TabIndex = 10;
@@ -120,25 +128,77 @@
             this.BrowseFilesButton.UseVisualStyleBackColor = true;
             this.BrowseFilesButton.Click += new System.EventHandler(this.BrowseFilesButton_Click);
             // 
+            // OpenFileButton
+            // 
+            this.OpenFileButton.Location = new System.Drawing.Point(9, 62);
+            this.OpenFileButton.Name = "OpenFileButton";
+            this.OpenFileButton.Size = new System.Drawing.Size(79, 23);
+            this.OpenFileButton.TabIndex = 11;
+            this.OpenFileButton.Text = "Open File";
+            this.OpenFileButton.UseVisualStyleBackColor = true;
+            // 
+            // OptionsLabel
+            // 
+            this.OptionsLabel.AutoSize = true;
+            this.OptionsLabel.Location = new System.Drawing.Point(9, 64);
+            this.OptionsLabel.Name = "OptionsLabel";
+            this.OptionsLabel.Size = new System.Drawing.Size(43, 13);
+            this.OptionsLabel.TabIndex = 12;
+            this.OptionsLabel.Text = "Options";
+            // 
+            // OpenSelectedFileButton
+            // 
+            this.OpenSelectedFileButton.Location = new System.Drawing.Point(171, 113);
+            this.OpenSelectedFileButton.Name = "OpenSelectedFileButton";
+            this.OpenSelectedFileButton.Size = new System.Drawing.Size(75, 23);
+            this.OpenSelectedFileButton.TabIndex = 13;
+            this.OpenSelectedFileButton.Text = "Open File";
+            this.OpenSelectedFileButton.UseVisualStyleBackColor = true;
+            this.OpenSelectedFileButton.Click += new System.EventHandler(this.OpenSelectedFileButton_Click);
+            // 
+            // FileOptionsGroupBox
+            // 
+            this.FileOptionsGroupBox.Controls.Add(this.SelectedFileLabel);
+            this.FileOptionsGroupBox.Controls.Add(this.OpenSelectedFileButton);
+            this.FileOptionsGroupBox.Controls.Add(this.ArchiveButton);
+            this.FileOptionsGroupBox.Controls.Add(this.OptionsLabel);
+            this.FileOptionsGroupBox.Controls.Add(this.ExtractButton);
+            this.FileOptionsGroupBox.Controls.Add(this.EncryptButton);
+            this.FileOptionsGroupBox.Controls.Add(this.DecryptButton);
+            this.FileOptionsGroupBox.Controls.Add(this.AttributesButton);
+            this.FileOptionsGroupBox.Controls.Add(this.SelectedFileName);
+            this.FileOptionsGroupBox.Location = new System.Drawing.Point(12, 147);
+            this.FileOptionsGroupBox.Name = "FileOptionsGroupBox";
+            this.FileOptionsGroupBox.Size = new System.Drawing.Size(385, 162);
+            this.FileOptionsGroupBox.TabIndex = 14;
+            this.FileOptionsGroupBox.TabStop = false;
+            // 
+            // SearchGroupBox
+            // 
+            this.SearchGroupBox.Controls.Add(this.SearchLabel);
+            this.SearchGroupBox.Controls.Add(this.FileSearchBox);
+            this.SearchGroupBox.Controls.Add(this.OpenFileButton);
+            this.SearchGroupBox.Controls.Add(this.BrowseFilesButton);
+            this.SearchGroupBox.Location = new System.Drawing.Point(12, 31);
+            this.SearchGroupBox.Name = "SearchGroupBox";
+            this.SearchGroupBox.Size = new System.Drawing.Size(385, 110);
+            this.SearchGroupBox.TabIndex = 15;
+            this.SearchGroupBox.TabStop = false;
+            // 
             // SanityArchiver
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(534, 140);
-            this.Controls.Add(this.BrowseFilesButton);
-            this.Controls.Add(this.SelectedFileLabel);
-            this.Controls.Add(this.SelectedFileName);
-            this.Controls.Add(this.AttributesButton);
-            this.Controls.Add(this.DecryptButton);
-            this.Controls.Add(this.SearchLabel);
-            this.Controls.Add(this.FileSearchBox);
-            this.Controls.Add(this.EncryptButton);
-            this.Controls.Add(this.ExtractButton);
-            this.Controls.Add(this.ArchiveButton);
+            this.ClientSize = new System.Drawing.Size(409, 461);
+            this.Controls.Add(this.SearchGroupBox);
+            this.Controls.Add(this.FileOptionsGroupBox);
             this.Name = "SanityArchiver";
             this.Text = "SanityArchiver";
+            this.FileOptionsGroupBox.ResumeLayout(false);
+            this.FileOptionsGroupBox.PerformLayout();
+            this.SearchGroupBox.ResumeLayout(false);
+            this.SearchGroupBox.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -154,6 +214,11 @@
         private System.Windows.Forms.TextBox SelectedFileName;
         private System.Windows.Forms.Label SelectedFileLabel;
         private System.Windows.Forms.Button BrowseFilesButton;
+        private System.Windows.Forms.Button OpenFileButton;
+        private System.Windows.Forms.Label OptionsLabel;
+        private System.Windows.Forms.Button OpenSelectedFileButton;
+        private System.Windows.Forms.GroupBox FileOptionsGroupBox;
+        private System.Windows.Forms.GroupBox SearchGroupBox;
     }
 }
 
--- SanityArchiver/SanityArchiver/SanityArchiver.cs
+++ SanityArchiver/SanityArchiver/SanityArchiver.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +13,71 @@ namespace SanityArchiver
 {
     public partial class SanityArchiver : Form
     {
+        public FileInfo SelectedFile { get; set; }
+
         public SanityArchiver()
         {
             InitializeComponent();
         }
 
+        private void SetText(string text, TextBox textBox)
+        {
+            textBox.Text = text;
+        }
+
         private void BrowseFilesButton_Click(object sender, EventArgs e)
         {
-            DialogResult dialogResult = new OpenFileDialog().ShowDialog();
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                SetText(openFileDialog.FileName, SelectedFileName);
+            }
+        }
+
+        private void SelectedFileChanged(object sender, EventArgs e)
+        {
+            Button[] buttons = { EncryptButton, DecryptButton, ArchiveButton, ExtractButton,
+                                 OpenSelectedFileButton, AttributesButton };
+
+            foreach (Button button in buttons)
+            {
+                button.Enabled = true;
+            }
+
+            if (SelectedFileName != null)
+            {
+                try
+                {
+                    SelectedFile = new FileInfo(SelectedFileName.Text);
+                    if (SelectedFile.Attributes.HasFlag(FileAttributes.Encrypted))
+                    {
+                        EncryptButton.Enabled = false;
+                        DecryptButton.Enabled = true;
+                    }
+                    if (SelectedFile.Attributes.HasFlag(FileAttributes.Archive))
+                    {
+                        ArchiveButton.Enabled = false;
+                        ExtractButton.Enabled = true;
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    MessageBox.Show($"Error locating file.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+
+                    foreach (Button button in buttons)
+                    {
+                        button.Enabled = false;
+                    }
+                }
+            }
+        }
+
+        private void OpenSelectedFileButton_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(SelectedFile.FullName);
         }
     }
 }
